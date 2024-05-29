b2674d809668ce7b06ffa40b7168b4e896cbb14a
reda cherfaoui <redacherfaoui@gmail.com>
2024-05-29 20:36:30 +0100

    change footer

--- src/components/starter/footer/footer.tsx
+++ src/components/starter/footer/footer.tsx
@@ -8,6 +8,12 @@ export default component$(() => {
   return (
     <footer>
       <div class="container">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus ipsam
+          explicabo, harum sequi distinctio repudiandae obcaecati cupiditate
+          praesentium officia cum? Neque accusantium repellat deserunt
+          accusamus, quos adipisci optio ipsum autem?
+        </p>
         <a href="https://www.builder.io/" target="_blank" class={styles.anchor}>
           <span>Made with ♡ by Builder.io</span>
           <span class={styles.spacer}>|</span>
